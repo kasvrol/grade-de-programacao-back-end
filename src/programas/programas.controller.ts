e5fdf17ccbdb4ab9bbd54ef2eb457aa541e6a12a
kasvrol <dovalekarolina@gmail.com>
2023-09-10 18:29:09 -0300

    feat(api): Adicionar método POST para armazenar programações referentes a data

--- src/programas/programas.controller.ts
+++ src/programas/programas.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Post } from '@nestjs/common';
 import { ProgramaService } from '../programas/shared/programa.service/programa.service';
 
 @Controller('programas')
@@ -6,7 +6,12 @@ export class ProgramasController {
   constructor(private readonly externalApiService: ProgramaService) {}
 
   @Get()
-  async fetchDataFromExternalApi(@Query('data') data: string): Promise<any> {
-    return this.externalApiService.fetchDataFromExternalApi(data);
+  async requisitarDadosDaAPIGlobo(@Query('data') data: string): Promise<any> {
+    return this.externalApiService.requisitarDadosDaAPIGlobo(data);
+  }
+
+  @Post()
+  async importarDadosDaAPIGlobo(@Query('data') data: string): Promise<void> {
+    await this.externalApiService.requisitarDadosESalvarNoBD(data);
   }
 }
--- src/programas/shared/programa.service/programa.service.ts
+++ src/programas/shared/programa.service/programa.service.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 @Injectable()
 export class ProgramaService {
-  async fetchDataFromExternalApi(data): Promise<any> {
+  async requisitarDadosDaAPIGlobo(data): Promise<any> {
     try {
       const id = 1337;
       const response = await axios.get(
@@ -14,6 +14,21 @@ export class ProgramaService {
     } catch (error) {
       console.log(error);
       throw new Error('Erro ao consumir a API externa');
+    }
+  }
+
+  async requisitarDadosESalvarNoBD(data): Promise<void> {
+    try {
+      const id = 1337;
+      const response = await axios.get(
+        `https://epg-api.video.globo.com/programmes/${id}?date=${data}`,
+      );
+      const programacao = response.data;
+      console.log(programacao);
+    } catch (error) {
+      throw new Error(
+        'Erro ao consumir a API externa e salvar os dados no banco de dados',
+      );
     }
   }
 }
