48e2a007bec0cd638dd38ae3888cf1d251b1774e
kasvrol <dovalekarolina@gmail.com>
2023-09-10 16:40:34 -0300

    feat cors

---
+++ src/cors.config.ts
@@ -0,0 +1,8 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+export const corsOptions: CorsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { corsOptions } from './cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   await app.listen(3002);
 }
 bootstrap();
