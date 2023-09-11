5bb055dd86074f34fb46a0a0afbf67bec8a0fa21
kasvrol <dovalekarolina@gmail.com>
2023-09-11 10:20:55 -0300

    feat(service) Adicionar requisição de método Post com base na data dada na url

--- src/programas/programas.controller.ts
+++ src/programas/programas.controller.ts
@@ -1,17 +1,12 @@
-import { Controller, Get, Query, Post } from '@nestjs/common';
+import { Controller, Query, Post } from '@nestjs/common';
 import { ProgramaService } from '../programas/shared/programa.service/programa.service';
 
 @Controller('programas')
 export class ProgramasController {
-  constructor(private readonly externalApiService: ProgramaService) {}
-
-  @Get()
-  async requisitarDadosDaAPIGlobo(@Query('data') data: string): Promise<any> {
-    return this.externalApiService.requisitarDadosDaAPIGlobo(data);
-  }
+  constructor(private readonly ApiDeProgramacaoGlobo: ProgramaService) {}
 
   @Post()
   async importarDadosDaAPIGlobo(@Query('data') data: string): Promise<void> {
-    await this.externalApiService.requisitarDadosESalvarNoBD(data);
+    await this.ApiDeProgramacaoGlobo.requisitarDadosESalvarNoBD(data);
   }
 }
