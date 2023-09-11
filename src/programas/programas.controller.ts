9900938a53e1395661c21aa6f66226d4bf1ad5d0
kasvrol <dovalekarolina@gmail.com>
2023-09-11 10:55:22 -0300

    feat(api): Implementar verificação de dados existentes antes do POST

--- src/programas/programas.controller.ts
+++ src/programas/programas.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Query, Post, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Param,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
 import { ProgramaService } from '../programas/shared/programa.service/programa.service';
+import { Response } from 'express';
 
 @Controller('programas')
 export class ProgramasController {
@@ -13,7 +22,22 @@ export class ProgramasController {
   }
 
   @Post()
-  async importarDadosDaAPIGlobo(@Query('data') data: string): Promise<void> {
+  async importarDadosDaAPIGlobo(
+    @Query('data') data: string,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const verificaProgramacao =
+      await this.ApiDeProgramacaoGlobo.requisitarDadosDaAPIGlobo(data);
+
+    if (verificaProgramacao) {
+      return res
+        .status(HttpStatus.CONFLICT)
+        .json({ mensagem: 'A programação desta data já existe' });
+    }
+
     await this.ApiDeProgramacaoGlobo.requisitarDadosESalvarNoBD(data);
+    return res
+      .status(HttpStatus.CREATED)
+      .json({ mensagem: 'Programação criada com sucesso' });
   }
 }
