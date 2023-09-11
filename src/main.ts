13364b34c59f80673b5d5695e49e862a7b558b21
kasvrol <dovalekarolina@gmail.com>
2023-09-11 05:16:12 -0300

    feat(main) adicionar função para fechar bd

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(corsOptions);
   await app.listen(3002);
+  app.enableShutdownHooks();
 }
 bootstrap();
