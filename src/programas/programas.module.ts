8512753308c80c63699c44ca2549b7ece5d3b505
kasvrol <dovalekarolina@gmail.com>
2023-09-11 05:14:34 -0300

    feat(service) Configurar service prisma

--- src/programas/programas.module.ts
+++ src/programas/programas.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProgramasController } from './programas.controller';
 import { ProgramaService } from './shared/programa.service/programa.service';
+import { PrismaService } from '../database/PrismaService';
 
 @Module({
   controllers: [ProgramasController],
-  providers: [ProgramaService],
+  providers: [ProgramaService, PrismaService],
 })
 export class ProgramasModule {}
