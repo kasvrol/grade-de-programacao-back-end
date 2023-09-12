c2cf422276d99d761e5724333c171b8f263617cf
kasvrol <dovalekarolina@gmail.com>
2023-09-12 07:50:24 -0300

    refactor(api): Refatorar controladores da API

--- src/programas/programas.controller.ts
+++ src/programas/programas.controller.ts
@@ -19,37 +19,34 @@ export class ProgramasController {
   async buscarDadosPorData(
     @Param('data') data: string,
     @Res() res: Response,
-  ): Promise<Response> {
-    const dados =
-      await this.ApiDeProgramacaoGlobo.requisitarDadosDaAPIGlobo(data);
-
-    if (dados) {
-      return res.status(HttpStatus.OK).json({ programme: dados });
-    }
-
-    return res
-      .status(HttpStatus.NOT_FOUND)
-      .json({ mensagem: 'A programação desta data não foi criada ainda' });
+  ): Promise<void> {
+    await this.ApiDeProgramacaoGlobo.requisitarDadosDaAPIGlobo(data)
+      .then((dados) => {
+        return res.status(HttpStatus.OK).json({ programme: dados });
+      })
+      .catch(() => {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ mensagem: 'Programação já existe' });
+      });
   }
 
   @Post()
   async importarDadosDaAPIGlobo(
     @Query('data') data: string,
     @Res() res: Response,
-  ): Promise<Response> {
-    const verificaProgramacao =
-      await this.ApiDeProgramacaoGlobo.requisitarDadosDaAPIGlobo(data);
-
-    if (verificaProgramacao) {
-      return res
-        .status(HttpStatus.CONFLICT)
-        .json({ mensagem: 'A programação desta data já existe' });
-    }
-
-    await this.ApiDeProgramacaoGlobo.requisitarDadosESalvarNoBD(data);
-    return res
-      .status(HttpStatus.CREATED)
-      .json({ mensagem: 'Programação criada com sucesso' });
+  ): Promise<void> {
+    await this.ApiDeProgramacaoGlobo.requisitarDadosESalvarNoBD(data)
+      .then(() => {
+        return res
+          .status(HttpStatus.OK)
+          .json({ mensagem: 'Programação foi registrada com sucesso' });
+      })
+      .catch(() => {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ mensagem: 'Programação já existe' });
+      });
   }
 
   @Delete(':data')
